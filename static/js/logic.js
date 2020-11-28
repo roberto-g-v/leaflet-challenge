8b9fad5458a5d4b1dc48e395a692e14aee461161
roberto-g-v <roberto.gonzalez.vallejo@gmail.com>
2020-11-28 01:50:33 -0600

    Final

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,51 +1,120 @@
-// Creating map object
-var myMap = L.map("map", {
-    center: [34.0522, -118.2437],
-    zoom: 2
-  });
+/********************
+LOGIC AND PLUG INS
+*********************/
+/// Store our API endpoint inside queryUrl
+var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
+
+var query2 = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.geojson"
+
+// Perform a GET request to the query URL
+d3.json(queryUrl, function(data) {
+  // Once we get a response, send the data.features object to the createFeatures function
+  createFeatures(data.features);
+});
+
+function createFeatures(earthquakeData) {
+
+
+  // Give each feature a popup describing the place and time of the earthquake
+  function onEachFeature(feature, layer) {
+    layer.bindPopup("<h3>" + feature.properties.place +
+      "</h3><hr><p>" + new Date(feature.properties.time) + "</p>" +
+      "</h3><hr><p>Magnitude: " + feature.properties.mag + "</p>");
+  }
+
   
-  // Adding tile layer
-  L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+
+  // Create a GeoJSON layer containing the features array on the earthquakeData object
+  // Run the onEachFeature function once for each piece of data in the array
+  var earthquakes = L.geoJSON(earthquakeData, {
+    onEachFeature: onEachFeature,
+    pointToLayer: function (feature, latlng) {
+      var color;
+      var r = 255;
+      var g = Math.floor(255-30*feature.properties.mag);
+      var b = Math.floor(255-30*feature.properties.mag);
+      color= "rgb("+r+" ,"+g+","+ b+")"
+      
+      var geojsonMarkerOptions = {
+        radius: 4*feature.properties.mag,
+        fillColor: color,
+        color: "red",
+        weight: 1,
+        opacity: 1,
+        fillOpacity: 0.7
+      };
+      return L.circleMarker(latlng, geojsonMarkerOptions);
+    }
+  });
+
+
+  // Sending our earthquakes layer to the createMap function
+  createMap(earthquakes);
+  
+}
+
+function createMap(earthquakes) {
+
+  // Define streetmap and darkmap layers
+  var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
     attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
     tileSize: 512,
     maxZoom: 18,
     zoomOffset: -1,
     id: "mapbox/streets-v11",
-    accessToken: API_KEY
-  }).addTo(myMap);
+    accessToken: API_KEY});
+
+// Define a baseMaps object to hold our base layers
+  var baseMaps = {
+    "Street Map": streetmap
+  };
+
+  // Create overlay object to hold our overlay layer
+  var overlayMaps = {
+    Earthquakes: earthquakes
+  };
   
-// Store API query variables
-var baseURL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
-var date = "$where=time between'1605939415600' and '1605936214174'";
-var mag = "&mag=>1";
-var limit = "&$limit=10000";
+  // Creating map object
+  var myMap = L.map("map", {
+    center: [34.0522, -118.2437],
+    zoom: 2,
+    layers: [streetmap, earthquakes]
+  });
 
-// Assemble API query URL
-var url = baseURL + date + mag + limit;
+// Define color
+function getColor(d) {
+    return d < 1 ? 'rgb(255,255,255)' :
+          d < 2  ? 'rgb(255,225,225)' :
+          d < 3  ? 'rgb(255,195,195)' :
+          d < 4  ? 'rgb(255,165,165)' :
+          d < 5  ? 'rgb(255,135,135)' :
+          d < 6  ? 'rgb(255,105,105)' :
+          d < 7  ? 'rgb(255,75,75)' :
+          d < 8  ? 'rgb(255,45,45)' :
+          d < 9  ? 'rgb(255,15,15)' :
+                      'rgb(255,0,0)';
+}
+  // Create a legend to display information about our map
+  var legend = L.control({position: 'bottomright'});
 
-// Grab the data with d3
-d3.json(url, function(response) {
+  legend.onAdd = function (map) {
+  
+      var div = L.DomUtil.create('div', 'info legend'),
+      grades = [0, 1, 2, 3, 4, 5, 6, 7, 8],
+      labels = [];
 
-  // Create a new marker cluster group
-  var markers = L.markerClusterGroup();
-
-  // Loop through data
-  for (var i = 0; i < response.length; i++) {
-
-    // Set the data location property to a variable
-    var geometry = response[i].geometry;
-
-    // Check for location property
-    if (geometry) {
-
-      // Add a new marker to the cluster group and bind a pop-up
-      markers.addLayer(L.marker([geometry.coordinates[1], geometry.coordinates[0]])
-        .bindPopup(response[i].descriptor));
-    }
-
+      div.innerHTML+='Magnitude<br><hr>'
+  
+      // loop through our density intervals and generate a label with a colored square for each interval
+      for (var i = 0; i < grades.length; i++) {
+          div.innerHTML +=
+              '<i style="background:' + getColor(grades[i] + 1) + '">&nbsp&nbsp&nbsp&nbsp</i> ' +
+              grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
   }
+  
+  return div;
+  };
+  
+  legend.addTo(myMap);
 
-  // Add our marker cluster layer to the map
-  myMap.addLayer(markers);
-
-});
+}
